7e473a50b94e4b9b73b1f39627fc2c1624fb87f8
TariqMBooker <55111113+TariqMBooker@users.noreply.github.com>
2026-06-06 14:54:17 -0400

    Create usingAVariable.java

---
+++ usingAVariable.java
@@ -0,0 +1,14 @@
+public class UsingAVariable
+{
+    public static void main(String[] args)
+    {
+        int x;
+        x = 10;
+        x = 5;
+        x = x + 1;
+
+        int myFinalScore = 10;
+
+        System.out.println(myFinalScore);
+    }
+}
